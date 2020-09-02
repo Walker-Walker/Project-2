e5ec05396f804f198a4c186ce5fe49e5673939e1
Walker-Walker <awalker0@gmail.com>
2020-09-01 21:03:01 -0700

    adding demo seeds for map functionality

--- db/schema.sql
+++ db/schema.sql
@@ -1,3 +1,8 @@
-DROP DATABASE IF EXISTS tech_blog_db;
+DROP DATABASE IF EXISTS shelter_db;
 
-CREATE DATABASE tech_blog_db;
+CREATE DATABASE shelter_db;
+
+CREATE TABLE shelters (
+    city varchar(50),
+    address varchar(50)
+)
---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+use shelter_db;
+
+INSERT INTO shelters 
+(city, address)
+VALUES
+(San Ramon, 12501 Alcosta Blvd ),
+(Napa, 2277 Napa Vallejo Hwy)
